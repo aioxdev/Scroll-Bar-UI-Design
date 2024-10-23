f453f63d6e9a4232c9a6834d138363c4658fa47f
aioxdev <aioxdev08@gmail.com>
2024-10-23 06:31:14 +0300

    ScrollBar UI Design

---
+++ ScrollUI/ScrollUI/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ScrollUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
